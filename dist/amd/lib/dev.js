8bfe8a2fd6b9d532467921f79e382e5fef5394cf
Bradley Momberger <brad@bitovi.com>
2017-09-20 17:01:22 -0400

    1.0.1

---
+++ dist/amd/lib/dev.js
@@ -0,0 +1,69 @@
+/*can-test-helpers@1.0.0#lib/dev*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-util/js/dev',
+    'can-util/js/make-array',
+    'can-util/js/global'
+], function (require, exports, module) {
+    (function (global) {
+        var dev = require('can-util/js/dev');
+        var makeArray = require('can-util/js/make-array');
+        var GLOBAL = require('can-util/js/global');
+        function makeExpectation(type) {
+            var original;
+            var expectedResults = [];
+            function stubbed() {
+                var message = makeArray(arguments).map(function (token) {
+                    if (typeof token !== 'string' && token.message) {
+                        return token.message;
+                    } else {
+                        return token;
+                    }
+                }).join(' ');
+                expectedResults.forEach(function (expected) {
+                    var matched = typeof expected.source === 'string' ? message === expected.source : expected.source.test(message);
+                    if (matched) {
+                        expected.count++;
+                    }
+                    if (typeof expected.fn === 'function') {
+                        expected.fn.call(null, message, matched);
+                    }
+                });
+            }
+            return function (expected, fn) {
+                var matchData = {
+                    source: expected,
+                    fn: fn,
+                    count: 0
+                };
+                expectedResults.push(matchData);
+                if (!original) {
+                    original = dev[type];
+                    dev[type] = stubbed;
+                }
+                return function () {
+                    expectedResults.splice(expectedResults.indexOf(matchData), 1);
+                    if (original && expectedResults.length < 1) {
+                        dev[type] = original;
+                        original = null;
+                    }
+                    return matchData.count;
+                };
+            };
+        }
+        module.exports = {
+            willWarn: makeExpectation('warn'),
+            willError: makeExpectation('error'),
+            devOnlyTest: function () {
+                var global = GLOBAL();
+                if (!global.System || !global.System.env || global.System.env.indexOf('production') < 0) {
+                    global.test.apply(null, arguments);
+                }
+            }
+        };
+    }(function () {
+        return this;
+    }()));
+});
