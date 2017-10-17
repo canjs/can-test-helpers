151b93fcc11f282429545bb76cbdac3a496d8308
Bradley Momberger <brad@bitovi.com>
2017-10-17 16:54:30 -0400

    Remove reference to missing make-mock-component file

--- can-test-helpers.js
+++ can-test-helpers.js
@@ -1,9 +1,7 @@
 //# can-test-helpers
 
 var dev = require('can-test-helpers/lib/dev');
-var makeMockComponent = require('can-test-helpers/lib/make-mock-component');
 
 module.exports = {
-	dev: dev,
-  makeMockComponent: makeMockComponent
+	dev: dev
 };
