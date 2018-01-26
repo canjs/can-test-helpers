eeeeb319ec73c4072a16fbbf317039e462e35ff0
Justin Meyer <justinbmeyer@gmail.com>
2018-01-26 14:13:19 -0600

    1.1.1

---
+++ dist/amd/can-test-helpers.js
@@ -0,0 +1,10 @@
+/*can-test-helpers@1.1.0#can-test-helpers*/
+define([
+    'require',
+    'exports',
+    'module',
+    './lib/dev'
+], function (require, exports, module) {
+    var dev = require('./lib/dev');
+    module.exports = { dev: dev };
+});
---
+++ dist/cjs/can-test-helpers.js
@@ -0,0 +1,3 @@
+/*can-test-helpers@1.1.0#can-test-helpers*/
+var dev = require('./lib/dev.js');
+module.exports = { dev: dev };
