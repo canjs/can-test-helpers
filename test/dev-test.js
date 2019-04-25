4251f601d2013e353d3604306ccea080a17cc3ea
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-04-26 00:03:30 +0100

    Upgrade to QUnit2

--- test/dev-test.js
+++ test/dev-test.js
@@ -5,67 +5,67 @@ var global = require("can-global")();
 
 QUnit.module("can-test-helpers/dev");
 
-QUnit.test("willWarn stubs out dev.warn until restore is called", function() {
+QUnit.test("willWarn stubs out dev.warn until restore is called", function(assert) {
 	var _warn = dev.warn;
 
 	var restore = devUtils.willWarn();
 
-	QUnit.notEqual(_warn, dev.warn, "warn function stubbed");
+	assert.notEqual(_warn, dev.warn, "warn function stubbed");
 
 	restore();
 
-	QUnit.equal(_warn, dev.warn, "warn function unstubbed");
+	assert.equal(_warn, dev.warn, "warn function unstubbed");
 });
 
-QUnit.test("willWarn returns restore function", function() {
+QUnit.test("willWarn returns restore function", function(assert) {
 	var restore = devUtils.willWarn();
 
-	QUnit.equal(typeof restore, "function", "function returned");
+	assert.equal(typeof restore, "function", "function returned");
 
 	restore();
 });
 
-QUnit.test("willWarn restore function gives zero result if no warnings.", function() {
+QUnit.test("willWarn restore function gives zero result if no warnings.", function(assert) {
 	var restore = devUtils.willWarn();
 
-	QUnit.equal(restore(), 0, "Zero incidents counted");
+	assert.equal(restore(), 0, "Zero incidents counted");
 });
 
-QUnit.test("willWarn matches strings", function() {
+QUnit.test("willWarn matches strings", function(assert) {
 	var restore = devUtils.willWarn("foo");
 
 	dev.warn("foo");
 
-	QUnit.equal(restore(), 1, "Correct number of warnings reported");
+	assert.equal(restore(), 1, "Correct number of warnings reported");
 });
 
-QUnit.test("willWarn matches regex", function() {
+QUnit.test("willWarn matches regex", function(assert) {
 	var restore = devUtils.willWarn(/foo/);
 
 	dev.warn("foo bar");
 
-	QUnit.equal(restore(), 1, "Correct number of warnings reported");
+	assert.equal(restore(), 1, "Correct number of warnings reported");
 });
 
 
-QUnit.test("willWarn matches multiple warnings", function() {
+QUnit.test("willWarn matches multiple warnings", function(assert) {
 	var restore = devUtils.willWarn(/foo/);
 	var restore2 = devUtils.willWarn("foo bar");
 
 	dev.warn("foo bar");
 	dev.warn("foo baz");
 
-	QUnit.equal(restore(), 2, "Correct number of warnings reported for regex");
-	QUnit.equal(restore2(), 1, "Correct number of warnings reported for string");
+	assert.equal(restore(), 2, "Correct number of warnings reported for regex");
+	assert.equal(restore2(), 1, "Correct number of warnings reported for string");
 });
 
-QUnit.test("willWarn callback fires with string and match on ALL warnings", function() {
-	expect(4);
+QUnit.test("willWarn callback fires with string and match on ALL warnings", function(assert) {
+	assert.expect(4);
 	var restore = devUtils.willWarn(/foo/, function(message, matched) {
-		QUnit.ok(matched, "/foo/ regex match");
+		assert.ok(matched, "/foo/ regex match");
 	});
 	var restore2 = devUtils.willWarn("foo bar", function(message, matched) {
-		QUnit.ok(message, "string match: " + matched);
+		assert.ok(message, "string match: " + matched);
 	});
 
 	dev.warn("foo bar");
@@ -75,13 +75,13 @@ QUnit.test("willWarn callback fires with string and match on ALL warnings", func
 	restore2();
 });
 
-QUnit.test("willWarn works async as well", function() {
-	stop();
+QUnit.test("willWarn works async as well", function(assert) {
+	var done = assert.async();
 	var restore = devUtils.willWarn(/foo/, function(message, matched) {
-		QUnit.ok(matched, "/foo/ regex match");
+		assert.ok(matched, "/foo/ regex match");
 	});
 	var restore2 = devUtils.willWarn("foo bar", function(message, matched) {
-		QUnit.ok(message, "string match: " + matched);
+		assert.ok(message, "string match: " + matched);
 	});
 
 	setTimeout(function() {
@@ -91,91 +91,91 @@ QUnit.test("willWarn works async as well", function() {
 	setTimeout(function() {
 		restore();
 		restore2();
-		start();
+		done();
 	}, 50);
 });
 
-QUnit.test("willWarn stops counting after restore even if other warn stubs are still active", function() {
+QUnit.test("willWarn stops counting after restore even if other warn stubs are still active", function(assert) {
 	var restore = devUtils.willWarn(/foo/);
 	var restore2 = devUtils.willWarn("foo bar");
 
 	dev.warn("foo baz");
-	QUnit.equal(restore(), 1, "Correct number of warnings reported for regex");
+	assert.equal(restore(), 1, "Correct number of warnings reported for regex");
 	dev.warn("foo bar");
 
-	QUnit.equal(restore(), 1, "Same number of warnings reported for regex");
-	QUnit.equal(restore2(), 1, "Correct number of warnings reported for string");
+	assert.equal(restore(), 1, "Same number of warnings reported for regex");
+	assert.equal(restore2(), 1, "Correct number of warnings reported for string");
 });
 
-QUnit.test("willError stubs out dev.error until restore is called", function() {
+QUnit.test("willError stubs out dev.error until restore is called", function(assert) {
 	var _error = dev.error;
 
 	var restore = devUtils.willError();
 
-	QUnit.notEqual(_error, dev.error, "error function stubbed");
+	assert.notEqual(_error, dev.error, "error function stubbed");
 
 	restore();
 
-	QUnit.equal(_error, dev.error, "error function unstubbed");
+	assert.equal(_error, dev.error, "error function unstubbed");
 });
 
-QUnit.test("willError returns restore function", function() {
+QUnit.test("willError returns restore function", function(assert) {
 	var restore = devUtils.willError();
 
-	QUnit.equal(typeof restore, "function", "function returned");
+	assert.equal(typeof restore, "function", "function returned");
 
 	restore();
 });
 
-QUnit.test("willError restore function gives zero result if no errors.", function() {
+QUnit.test("willError restore function gives zero result if no errors.", function(assert) {
 	var restore = devUtils.willError();
 
-	QUnit.equal(restore(), 0, "Zero incidents counted");
+	assert.equal(restore(), 0, "Zero incidents counted");
 });
 
-QUnit.test("willError matches strings", function() {
+QUnit.test("willError matches strings", function(assert) {
 	var restore = devUtils.willError("foo");
 
 	dev.error("foo");
 
-	QUnit.equal(restore(), 1, "Correct number of errors reported");
+	assert.equal(restore(), 1, "Correct number of errors reported");
 });
 
-QUnit.test("willError matches regex", function() {
+QUnit.test("willError matches regex", function(assert) {
 	var restore = devUtils.willError(/foo/);
 
 	dev.error("foo bar");
 
-	QUnit.equal(restore(), 1, "Correct number of errors reported");
+	assert.equal(restore(), 1, "Correct number of errors reported");
 });
 
-QUnit.test("willError matches error message for Error object", function() {
+QUnit.test("willError matches error message for Error object", function(assert) {
 	var restore = devUtils.willError("foo");
 
 	dev.error(new Error("foo"));
 
-	QUnit.equal(restore(), 1, "Correct number of errors reported");
+	assert.equal(restore(), 1, "Correct number of errors reported");
 });
 
 
-QUnit.test("willError matches multiple errors", function() {
+QUnit.test("willError matches multiple errors", function(assert) {
 	var restore = devUtils.willError(/foo/);
 	var restore2 = devUtils.willError("foo bar");
 
 	dev.error("foo bar");
 	dev.error("foo baz");
 
-	QUnit.equal(restore(), 2, "Correct number of errors reported for regex");
-	QUnit.equal(restore2(), 1, "Correct number of errors reported for string");
+	assert.equal(restore(), 2, "Correct number of errors reported for regex");
+	assert.equal(restore2(), 1, "Correct number of errors reported for string");
 });
 
-QUnit.test("willError callback fires with string and match on ALL errors", function() {
-	expect(4);
+QUnit.test("willError callback fires with string and match on ALL errors", function(assert) {
+	assert.expect(4);
 	var restore = devUtils.willError(/foo/, function(message, matched) {
-		QUnit.ok(matched, "/foo/ regex match");
+		assert.ok(matched, "/foo/ regex match");
 	});
 	var restore2 = devUtils.willError("foo bar", function(message, matched) {
-		QUnit.ok(message, "string match: " + matched);
+		assert.ok(message, "string match: " + matched);
 	});
 
 	dev.error("foo bar");
@@ -185,13 +185,13 @@ QUnit.test("willError callback fires with string and match on ALL errors", funct
 	restore2();
 });
 
-QUnit.test("willError works async as well", function() {
-	stop();
+QUnit.test("willError works async as well", function(assert) {
+	var done = assert.async();
 	var restore = devUtils.willError(/foo/, function(message, matched) {
-		QUnit.ok(matched, "/foo/ regex match");
+		assert.ok(matched, "/foo/ regex match");
 	});
 	var restore2 = devUtils.willError("foo bar", function(message, matched) {
-		QUnit.ok(message, "string match: " + matched);
+		assert.ok(message, "string match: " + matched);
 	});
 
 	setTimeout(function() {
@@ -201,24 +201,24 @@ QUnit.test("willError works async as well", function() {
 	setTimeout(function() {
 		restore();
 		restore2();
-		start();
+		done();
 	}, 50);
 });
 
-QUnit.test("willError stops counting after restore even if other error stubs are still active", function() {
+QUnit.test("willError stops counting after restore even if other error stubs are still active", function(assert) {
 	var restore = devUtils.willError(/foo/);
 	var restore2 = devUtils.willError("foo bar");
 
 	dev.error("foo baz");
-	QUnit.equal(restore(), 1, "Correct number of errors reported for regex");
+	assert.equal(restore(), 1, "Correct number of errors reported for regex");
 	dev.error("foo bar");
 
-	QUnit.equal(restore(), 1, "Same number of errors reported for regex");
-	QUnit.equal(restore2(), 1, "Correct number of errors reported for string");
+	assert.equal(restore(), 1, "Same number of errors reported for regex");
+	assert.equal(restore2(), 1, "Correct number of errors reported for string");
 });
 
 
-QUnit.test("willError and willWarn don't count each other's logging", function() {
+QUnit.test("willError and willWarn don't count each other's logging", function(assert) {
 	var restore = devUtils.willError(/foo/);
 	var restore2 = devUtils.willWarn(/bar/);
 
@@ -226,26 +226,27 @@ QUnit.test("willError and willWarn don't count each other's logging", function()
 	dev.warn("foo bar");
 	dev.warn("foo bar quux");
 
-	QUnit.equal(restore(), 1, "Correct number of errors reported");
-	QUnit.equal(restore2(), 2, "Correct number of warnings reported");
+	assert.equal(restore(), 1, "Correct number of errors reported");
+	assert.equal(restore2(), 2, "Correct number of warnings reported");
 });
 
 
-QUnit.test("devOnlyTest calls test() only when the environment is not production", function() {
+QUnit.test("devOnlyTest calls test() only when the environment is not production", function(assert) {
 	var oldEnv = System.env;
-	var oldTest = global.test;
-	expect(1);
+	var oldTest = global.QUnit.test;
+	assert.expect(2);
 
 	System.env = "window-development";
-	global.test = function(name) {
-		QUnit.equal(name, "foo", "development test function called");
+	global.QUnit.test = function(name, cb) {
+		assert.equal(typeof cb,  "function", "callback function");
+		assert.equal(name, "foo", "development test function called");
 	};
 
-	devUtils.devOnlyTest("foo");
+	devUtils.devOnlyTest("foo", function(){});
 
 	System.env = "window-production";
 
-	devUtils.devOnlyTest("foo");
+	devUtils.devOnlyTest("foo", function(){});
 
 	System.env = oldEnv;
 	global.test = oldTest;
