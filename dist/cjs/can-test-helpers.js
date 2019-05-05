b54f7092547eeae5dbc7ebb6f2a6465ae28746e6
Bradley Momberger <brad@bitovi.com>
2019-05-05 17:03:44 -0400

    1.1.3

---
+++ dist/cjs/can-test-helpers.js
@@ -0,0 +1,10 @@
+/*can-test-helpers@1.1.2#can-test-helpers*/
+define('can-test-helpers', [
+    'require',
+    'exports',
+    'module',
+    'can-test-helpers/lib/dev'
+], function (require, exports, module) {
+    var dev = require('can-test-helpers/lib/dev');
+    module.exports = { dev: dev };
+});
